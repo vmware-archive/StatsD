0fa01a6a9918a6fb06a2d5095b766f2557f9637d
Evan Pease <evanpease@ip-10-27-0-100.us-west-2.compute.internal>
2016-02-03 17:52:33 -0800

    updated namespace

--- backends/wavefront.js
+++ backends/wavefront.js
@@ -101,7 +101,6 @@ var flushStats = function wavefrontFlush(ts, metrics) {
     var value = counters[key];
 
     if (legacyNamespace === true) {
-      //statString += 'put stats_counts.' + key + ' ' + ts + ' ' + value + ' ' + tags.join(' ') + suffix;
       statString += 'stats_counts.' + key + ' ' + value + ' ' + ts + ' ' + tags.join(' ') + suffix;
     } else {
       statString += namespace.concat('count').join(".") + ' ' + value + ' ' + ts + ' ' + tags.join(' ') + suffix;
@@ -130,7 +129,6 @@ var flushStats = function wavefrontFlush(ts, metrics) {
     var strippedKey = stripTags(key)
 
     var namespace = gaugesNamespace.concat(strippedKey);
-    //statString += 'put ' + namespace.join(".") + ' ' + ts + ' ' + gauges[key] + ' ' + tags.join(' ') + suffix;
     statString += namespace.join(".") + ' ' + gauges[key] + ' ' + ts + ' ' + tags.join(' ') + suffix;
     numStats += 1;
   }
@@ -140,27 +138,21 @@ var flushStats = function wavefrontFlush(ts, metrics) {
     var strippedKey = stripTags(key)
 
     var namespace = setsNamespace.concat(strippedKey);
-    //statString += 'put ' + namespace.join(".") + '.count ' + ts + ' ' + sets[key].values().length + ' ' + tags.join(' ') + suffix;
     statString += namespace.join(".") + '.count ' + sets[key].values().length + ' ' + ts + ' ' + tags.join(' ') + suffix;
     numStats += 1;
   }
 
   var namespace = globalNamespace.concat('statsd');
   if (legacyNamespace === true) {
-    //statString += 'put statsd.numStats ' + ts + ' ' + numStats + suffix;
     statString += 'statsd.numStats ' + numStats + ' ' + ts + suffix;
-    //statString += 'put stats.statsd.wavefrontStats.calculationtime ' + ts + ' ' + (Date.now() - starttime) + suffix;
     statString += 'stats.statsd.wavefrontStats.calculationtime ' + (Date.now() - starttime) + ' ' + ts + suffix;
     for (key in statsd_metrics) {
       statString += 'stats.statsd.' + key + ' ' + statsd_metrics[key] + ' ' + ts + suffix;
     }
   } else {
-    //statString += 'put ' + namespace.join(".") + '.numStats ' + ts + ' ' + numStats + suffix;
     statString += namespace.join(".") + '.numStats ' + numStats + ' ' + ts + suffix;
-    //statString += 'put ' + namespace.join(".") + '.wavefrontStats.calculationtime ' + ts + ' ' + (Date.now() - starttime) + suffix;
     for (key in statsd_metrics) {
       var the_key = namespace.concat(key);
-      //statString += 'put ' + the_key.join(".") + ' ' + ts + ' ' + statsd_metrics[key] + suffix;
       statString += the_key.join(".") + ' ' + statsd_metrics[key] + ' ' + ts + suffix;
     }
   }
